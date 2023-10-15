32e628cb965847708759432b014d859fbebe96b7
remboinc <remboinc@mail.ru>
2023-10-15 21:45:18 +0300

    setup admin UI for likes.

--- property/admin.py
+++ property/admin.py
@@ -71,6 +71,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ('new_building',)
     list_filter = ('new_building', PriceCategoryFilter, RoomsNumberFilter, HasBalconyFilter,)
+    raw_id_fields = ('liked_by',)
 
 
 class ComplaintAdmin(admin.ModelAdmin):
--- property/models.py
+++ property/models.py
@@ -54,6 +54,12 @@ class Flat(models.Model):
         default=None,
         db_index=True
     )
+    liked_by = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name="liked_flats",
+        verbose_name='Кто лайкнул'
+    )
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
