3d23c010f7b2cefa9f9468c96452c71fb0d7c7e4
remboinc <remboinc@mail.ru>
2023-10-30 23:43:07 +0300

    Show owners flats

--- property/admin.py
+++ property/admin.py
@@ -70,6 +70,11 @@ class FlatInline(admin.TabularInline):
     raw_id_fields = ('flat',)
 
 
+class OwnerInline(admin.TabularInline):
+    model = Owner.flat.through
+    raw_id_fields = ('owner',)
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'owner', 'address', ]
     readonly_fields = ['created_at', ]
@@ -78,6 +83,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', PriceCategoryFilter, RoomsNumberFilter, HasBalconyFilter,)
     raw_id_fields = ('liked_by',)
+    inlines = [OwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
